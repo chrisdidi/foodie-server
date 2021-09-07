cac28452a7753782ce3d17ccadeee206ce5875b5
Man Hau Ling <chrisling.dev@gmail.com>
2021-09-08 07:17:02 +0800

    Config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import * as Joi from 'joi';
 import { ServerModule } from './server/server.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+      }),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
