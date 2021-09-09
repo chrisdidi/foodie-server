77ee9e5aa7bacd56a69df459a3e7c76b7b13f99b
Man Hau Ling <chrisling.dev@gmail.com>
2021-09-09 17:46:58 +0800

    Remove JWT Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,4 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -14,7 +9,6 @@ import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import { AuthModule } from './auth/auth.module';
-import { JwtMiddleware } from './jwt/jwt.middleware';
 
 @Module({
   imports: [
